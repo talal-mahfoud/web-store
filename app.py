87be2ee48cb0da8b453e293becf895f4acbc8967
talal-mahfoud <abushhiwat@gmail.com>
2025-06-27 17:00:05 +0300

    app.py

--- app.py
+++ app.py
@@ -180,4 +180,5 @@ def update_status(order_id):
 if __name__ =="__main__":
     with app.app_context():
          db.create_all()
-    app.run(host='0.0.0.0',port=18012)
+         port = int(os.environ.get("PORT", 18012))
+    app.run(host='0.0.0.0',port=port)
